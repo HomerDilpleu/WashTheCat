846bc45efc321808aa68b10a6e68bc7449a62164
HomerDilpleu <petitgigi78@yahoo.fr>
2025-08-27 22:43:46 +0200

    Start addng a end level animation

--- src/sprites/decoration.js
+++ src/sprites/decoration.js
@@ -4,29 +4,25 @@ game.sprites.decoration.create = function() {
     // End level
     p = game.sprites.decoration.cloneCreate()
     p.id = 'endLevel'
-    p.width = 200
-    p.height = 200
-    p.x = 200
-    p.y = 600
+    p.width = 700
+    p.height = 350
+    p.x = 630
+    p.y = 330
     //p.svg = new Path2D('M10 10 A19 19 0 1 0 40 10 L30 20 M40 10 L50 20 M0 0')
 
 }
 
 game.sprites.decoration.update = function () {
-    // End level
-    if (this.id == 'endLevel' && game.getLevelState() != 'running') {
-        this.isVisible = true
-    }
-    if (this.id == 'endLevel' && game.getLevelState() == 'running') {
-        this.isVisible = false
-    }
+    if (this.id == 'endLevel' && game.getLevelState() == 'running') {this.scaleY=0}
+    if (this.id == 'endLevel' && game.getLevelState() != 'running') {this.scaleY+=0.05}
+    if (this.id == 'endLevel' && this.scaleY > 1) {this.scaleY=1}
 }
 
 game.sprites.decoration.drawFunction = function (ctx) {
     // End level
-    if (this.id == 'endLevel' && this.isVisible) {
+    if (this.id == 'endLevel' && game.getLevelState() != 'running') {
         ctx.fillStyle = 'red'
-        ctx.fillRect(0,0,200,200)
+        ctx.fillRect(0,0,this.width,this.height)
     }
 
 }
